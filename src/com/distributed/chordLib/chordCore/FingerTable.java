d4117c2825d003b8500c7b70d8169c4ec42aaa54
pietro <pietro.daverio@mail.polimi.it>
2019-03-08 16:47:50 +0100

    [] updated FingerTable with new methods

--- src/com/distributed/chordLib/chordCore/FingerTable.java
+++ src/com/distributed/chordLib/chordCore/FingerTable.java
@@ -1,9 +1,25 @@
 package com.distributed.chordLib.chordCore;
 
 public class FingerTable {
-        Node[] fingers;
 
-        FingerTable (int nFingers){
+    private Node[] fingers;
+
+    FingerTable (int nFingers){
             fingers = new Node[nFingers];
     }
+
+    /**
+     * @return successor of this node
+     */
+    public Node getSuccessor(){ return fingers[0]; }
+
+    /**
+     * Find the Node among Node and its successors most appropriate for key
+     * @param key
+     * @return Node in FingerTable or Node itself
+     */
+    public Node[] getNextNode(String key) {
+        return null;
+    }
+
 }
