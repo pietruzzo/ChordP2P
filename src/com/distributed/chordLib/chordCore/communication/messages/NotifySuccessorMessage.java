af3149d3f0b58556bf24ba698673bc080e0bb0b0
pietro <pietro.daverio@mail.polimi.it>
2019-03-10 22:01:16 +0100

    [] Added new Messages

---
+++ src/com/distributed/chordLib/chordCore/communication/messages/NotifySuccessorMessage.java
@@ -0,0 +1,14 @@
+package com.distributed.chordLib.chordCore.communication.messages;
+
+import com.distributed.chordLib.chordCore.Node;
+
+import java.io.Serializable;
+
+public class NotifySuccessorMessage implements Serializable {
+
+    private static final long serialVersionUID = 40005L;
+
+    public final Node node;
+
+    public NotifySuccessorMessage(Node node) { this.node = node; }
+}
---
+++ src/com/distributed/chordLib/chordCore/communication/messages/PingMessage.java
@@ -0,0 +1,7 @@
+package com.distributed.chordLib.chordCore.communication.messages;
+
+public class PingMessage {
+
+    private static final long serialVersionUID = 40006L;
+
+}
