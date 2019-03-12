2fbee934ff78e7b72a3139be9e1b5155bf60a913
pietro <pietro.daverio@mail.polimi.it>
2019-03-12 15:20:13 +0100

    [] ComputationState added [] waitResponse defined

--- src/com/distributed/chordLib/chordCore/communication/SocketCommunication.java
+++ src/com/distributed/chordLib/chordCore/communication/SocketCommunication.java
@@ -2,9 +2,19 @@ package com.distributed.chordLib.chordCore.communication;
 
 import com.distributed.chordLib.chordCore.Node;
 import com.distributed.chordLib.chordCore.communication.messages.JoinResponseMessage;
+import com.distributed.chordLib.chordCore.communication.messages.ReqResp;
+
+import java.util.List;
+import java.util.Map;
 
 public class SocketCommunication implements CommCallInterface, CommCallbackInterface {
 
+    public static final int REQUEST_TIMEOUT = 3000;
+
+    //List of SocketNode
+    Map<Node, SocketNode> socketNodes;
+    //List of threads waiting for a response
+    List<ComputationState> waitingThreads;
 
     @Override
     public JoinResponseMessage join(String ip, String port) {
@@ -58,9 +68,37 @@ public class SocketCommunication implements CommCallInterface, CommCallbackInter
 
 
     /**
-     * Queue
+     * Send Request, suspend and queue thread, waiting for response
+     * @param requestMessage message that will be sent to receiver
+     * @param receiver receiver for the message
+     * @return Response message
      */
-    <T> T waitResponse(T responseMessage){
+    <T> T waitResponse(T requestMessage, SocketNode receiver){
+        //Send message on socket
+        //suspend current thread
+        //
         return null;
     }
+}
+
+/**
+ * Datastructure that store thread with request
+ */
+class ComputationState {
+
+    Thread thread;
+    ReqResp request;
+    ReqResp response;
+
+    ComputationState(Thread thread, ReqResp message) {
+        this.request = message;
+        this.thread = thread;
+        response = null;
+    }
+
+    //Suspend current thread
+    public void sleep(/*TODO: wait until response available or timeout elapse*/){
+        if (response == null) this.sleep();
+    }
+
 }
