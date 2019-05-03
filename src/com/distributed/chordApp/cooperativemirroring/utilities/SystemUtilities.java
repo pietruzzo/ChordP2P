ac9b6abc900fef0c6d23d735ee8dd52ae7b6fb20
Luca Lagni <lucalagni@gmail.com>
2019-05-03 12:13:10 +0200

    Added the automatic IP retrivement using the Java API

--- src/com/distributed/chordApp/cooperativemirroring/utilities/SystemUtilities.java
+++ src/com/distributed/chordApp/cooperativemirroring/utilities/SystemUtilities.java
@@ -1,8 +1,8 @@
 package com.distributed.chordApp.cooperativemirroring.utilities;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,9 +15,10 @@ public class SystemUtilities {
 
     /**
      * Method used for retriving the current machine's IP in an automatic way
+     *  This method uses the System commands for retriving the IP address of the current machine
      * @return
      */
-    public static synchronized String getThisMachineIP()
+    public static synchronized String getThisMachineIPSystemCommand()
     {
         String currentIP = null;
 
@@ -58,5 +59,55 @@ public class SystemUtilities {
 
 
         return currentIP;
+    }
+
+    /**
+     * Method used for automatically retriving the current host IP for the LAN
+     * This method uses the Java method for retriving the IP address of the current machine
+     * @return
+     */
+    public static synchronized String getThisMachineIP()
+    {
+        String result = null;
+
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+            while (networkInterfaces.hasMoreElements())
+            {
+                NetworkInterface ni = networkInterfaces.nextElement();
+                Enumeration<InetAddress> nias = ni.getInetAddresses();
+                while(nias.hasMoreElements())
+                {
+                    InetAddress ia= nias.nextElement();
+
+                    if (!ia.isLinkLocalAddress()
+                            && !ia.isLoopbackAddress()
+                            && ia instanceof Inet4Address)
+                    {
+                        String tmp = ia.getHostAddress();
+
+                        Pattern pt = Pattern.compile("192\\.168\\.[0-9]{1,3}\\.[0-9]{1,3}");
+                        Matcher mt = pt.matcher(tmp);
+                        Boolean found = mt.find();
+
+                        if(found) {
+                            result = tmp;
+                        }
+
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            System.err.println("Unable to retrive the current host IP, using the 127.0.0.1 instead");
+            result = "127.0.0.1";
+        }
+
+        if(result == null)
+        {
+           result = "127.0.0.1";
+        }
+
+        return result;
     }
 }
